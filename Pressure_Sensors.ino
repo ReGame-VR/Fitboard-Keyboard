f45d948f79feff8f5d94afa0e3599a99a6d38533
cslamp <lampasso.cul@husky.neu.edu>
2018-08-08 16:25:03 -0400

    Update Pressure_Sensors.ino

--- Pressure_Sensors.ino
+++ Pressure_Sensors.ino
@@ -2,14 +2,12 @@
 Circuit:
 -Flexsensor attached to A5 from 5V
 -10K resistor attached to A5 from ground
+
+Sensor 1 --> Analog Pin 1
+Sensor 2 --> Analog Pin 1
+Sensor 3 --> Analog Pin 1
+Sensor 4 --> Analog Pin 1
 */
-
-//Pin Number
-const int FlexPin_1 = A5; // Analog Pin
-const int FlexPin_2 = A4; // Analog Pin
-const int FlexPin_3 = A3; // Analog Pin
-const int FlexPin_4 = A2; // Analog Pin
-
 
 //Constants
 const int VCC = 5; //for voltage conversion, value of Arduino's volt supply
@@ -17,61 +15,29 @@ const double VoltCutoff = 3.75; //an arbratrary cut off for how hard sensor must
 
 void setup() {
   Serial.begin(9600); //Initialize serial monitor to print
-  pinMode(FlexPin_1, INPUT); //Set FlexPin_1 as an input
 }
 
 void loop() 
 {
-  //initialize switch variables to OFF (0)
-  int switch_1 = 0;
-  int switch_2 = 0;
-  int switch_3 = 0;
-  int switch_4 = 0;
-  
-  // Read the ADC (analog-to-digial converter), and calculate voltage from it
-  int flexADC_1 = analogRead(FlexPin_1);
-  //store voltage read in flexV_i
-  float flexV_1 = flexADC_1 * VCC / 1023.0;
+  //create array for switch (start as OFF (0) ) and voltage values
+  int switches[ ] = {0, 0, 0, 0};
+  //create array to hold values of voltage read from pressure sensors
+  float volts[sizeof(switches)/sizeof(int)];
 
-  int flexADC_2 = analogRead(FlexPin_2);
-  float flexV_2 = flexADC_2 * VCC / 1023.0;
-  
-  int flexADC_3 = analogRead(FlexPin_3);
-  float flexV_3 = flexADC_3 * VCC / 1023.0;
-
-  int flexADC_4 = analogRead(FlexPin_4);
-  float flexV_4 = flexADC_4 * VCC / 1023.0;
-
-//if voltage read is greater than cut off voltage, change corresponding switch to ON (1)
-if (flexV_1 >= VoltCutoff) {
-  switch_1 = 1;
-}
-if (flexV_2 >= VoltCutoff) {
-  switch_2 = 1;
-}
-if (flexV_3 >= VoltCutoff) {
-  switch_3 = 1;
-}
-if (flexV_4 >= VoltCutoff) {
-  switch_4 = 1;
-}
-
-//create an array to hold all voltage values
-float voltes[ ] = {flexV_1, flexV_2, flexV_3, flexV_4};
-//create an array to hold all switch values
-int switches[ ] = {switch_1, switch_2, switch_3, switch_4};
-
-//loop through array of switches to check which are ON
-for (int i=0; i < sizeof(switches); i++) {
-  if (switches[i] == 1) {
-    Serial.print("Switch ");
-    Serial.print(i+1);
-    Serial.print(": On... ");
-    Serial.println(voltes[i]);
+  //loop through the four sensors
+  for (int i=0; i < sizeof(switches)/sizeof(int); i++) {
+    int j = i+1; //add counter to correspond to pin the sensor is in
+    volts[i] = analogRead(j) * VCC / 1023.0; //read voltage of pin
+    //test if read voltage is higher than cut off voltage
+    if (volts[i] >= VoltCutoff){
+      switches[i] = 1; //if higher change the corresponding switch to ON (1)
+    }
+    //print the switch state and voltage read
+    Serial.print(switches[i]);
+    Serial.print("\t");
+    Serial.println(volts[i]);
   }
-  }
-
+//delay for stability
 Serial.println();
 delay(500); 
 }
-
