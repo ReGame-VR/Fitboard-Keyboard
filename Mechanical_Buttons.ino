11fb75b116abef406eb8b1b94f9a474e3877bd9f
cslamp <lampasso.cul@husky.neu.edu>
2018-08-08 17:00:57 -0400

    Update Mechanical_Buttons.ino

--- Mechanical_Buttons.ino
+++ Mechanical_Buttons.ino
@@ -2,76 +2,43 @@
 Circuit:
  -Button attached to digital pin 2 from 5V
  -10K resistor attached to pin 2 from ground
+
+ Button 1 --> Digital Pin 2
+ Button 2 --> Digital Pin 3
+ Button 3 --> Digital Pin 4
+ Button 4 --> Digital Pin 5
  */
 
-//Pin Number
-const int ButtonPin_1 = 1; //digital pin
-const int ButtonPin_2 = 2; //digital pin
-const int ButtonPin_3 = 3; //digital pin
-const int ButtonPin_4 = 4; //digital pin
-
-// variables will change:
-int ButtonPin_1State = 0; //intialize state
-int ButtonPin_2State = 0; //intialize state
-int ButtonPin_3State = 0; //intialize state
-int ButtonPin_4State = 0; //intialize state
+//create constant to hold number of buttons to use for loops
+const int noButtons = 4;
 
 void setup() {
   Serial.begin(9600); //Initialize serial monitor to print
-  pinMode(ButtonPin_1, INPUT); //initialize as an input
-  pinMode(ButtonPin_2, INPUT); //initialize as an input
-  pinMode(ButtonPin_3, INPUT); //initialize as an input
-  pinMode(ButtonPin_4, INPUT); //initialize as an input
+  //set pinModes for buttons as inputs
+  for (int j=1; j <= noButtons; j++) {
+    pinMode(j+1, INPUT);
+  }
 }
 
 void loop() {
-  //initialize switch variables to OFF (0)
-  int switch_1 = 0;
-  int switch_2 = 0;
-  int switch_3 = 0;
-  int switch_4 = 0;
-  
-  
-  //get Button states
-  ButtonPin_1State = digitalRead(ButtonPin_1);
-  ButtonPin_2State = digitalRead(ButtonPin_2);
-  ButtonPin_3State = digitalRead(ButtonPin_3);
-  ButtonPin_4State = digitalRead(ButtonPin_4);
-  
-  Serial.println("Checking:");
-  //if ButtonPin_1 is pressed print 'On'
-  if (ButtonPin_1State == HIGH) {
-    Serial.println("Button1: On");
-    //switch_1 = 1;
-  }
-  else if (ButtonPin_2State == HIGH) {
-    Serial.println("Button2: On");
-    //switch_2 = 1;
-  }
-  else if (ButtonPin_3State == HIGH) {
-    Serial.println("Button3: On");
-    //switch_3 = 1;
-  }
-  else if (ButtonPin_4State == HIGH) {
-    Serial.println("Button4: On");
-    //switch_4 = 1;
-  }
+  //create array to hold switch values. Intialize as OFF (0)
+  int switches[ ] = {0, 0, 0, 0};
 
-  
-  /*
-  //create an arry to hold all switch values
-  int switches[ ] = {switch_1, switch_2, switch_3, switch_4};
-
-  //loop through array of switches to check which are ON
-  for (int i=0; i < sizeof(switches); i++) {
-    if (switches[i] == 1) {
-      Serial.print("Button ");
-      Serial.print(i+1);
-      Serial.print(": On");
+  //loop through buttons to check if they are pressed
+  for (int i=0; i < noButtons; i++) {
+    //save current state in corresponding switches array slot
+    switches[i] = digitalRead(i+2);
+    Serial.print("Button ");
+    Serial.print(i+1);
+    //If they are pressed print ON. If they arent print OFF.
+    if (switches[i] == HIGH) {
+      
+      Serial.println(": ON.");
+    }
+    else {
+      Serial.println(": OFF.");
     }
   }
-    
-   */
   
   Serial.println();
   delay(500); //delay before looking for new value
