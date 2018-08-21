891e8972cde1fca8a75bc8827954d0fa7ce6ece6
cslamp <lampasso.cul@husky.neu.edu>
2018-08-21 16:28:23 -0400

    Update Proximity_Sensors.ino

--- Proximity_Sensors.ino
+++ Proximity_Sensors.ino
@@ -8,125 +8,75 @@
 
 //Digital Pin Numbers
 const int TRIG_1 = 3;
-const int TRIG_2 = 5;
-const int TRIG_3 = 7;
-const int TRIG_4 = 9;
 const int ECHO_1 = 4;
+const int TRIG_2 = 5;
 const int ECHO_2 = 6;
+const int TRIG_3 = 7;
 const int ECHO_3 = 8;
+const int TRIG_4 = 9;
 const int ECHO_4 = 10;
-
 //Constants
-MAX_DIST = 10; //create distance activation range in cm 
-MIN_DIST = 3;
+const int MAX_DIST = 10; //create distance activation range in cm 
+const int MIN_DIST = 3; //create minimum distance for activation
+const int noButtons = 4; //for loops
+
 
 void setup() {
   // initialize serial communication to pring
   Serial.begin(9600);
-  //Set digital pins as outputs or inputs
-  pinMode(TRIG_1, OUTPUT);
-  pinMode(TRIG_2, OUTPUT);
-  pinMode(TRIG_3, OUTPUT);
-  pinMode(TRIG_4, OUTPUT);
-  pinMode(ECHO_1, INPUT);
-  pinMode(ECHO_2, INPUT);
-  pinMode(ECHO_3, INPUT);
-  pinMode(ECHO_4, INPUT);
+  //Set digital pins as outputs / inputs
+  for (int k=3; k < 3 + (2*noButtons); k=k+2) {
+    pinMode(k, OUTPUT);
+    pinMode(k+1, INPUT);
+  }
 }
 
 void loop()
 {
-  //create variables for duration of the signal, 
-  long dur_1, dur_2, dur_3, dur_4;
-  // create variables for the distance:
-  long dist_1, dist_2, dist_3, dist_4;
-  // initialize switch positions as OFF (0)
-  int switch_1 = 0;
-  int switch_2 = 0;
-  int switch_3 = 0;
-  int switch_4 = 0;
+  int switches[ ] = {0,0,0,0}; //initialize switch array as OFF
+  long dists[ ] = {0,0,0,0}; //initialize distance array
 
-  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  digitalWrite(TRIG_1, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_1, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_1, LOW);
-
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  dur_1 = pulseIn(ECHO_1, HIGH);
-  
-  digitalWrite(TRIG_2, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_2, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_2, LOW);
-  
-  dur_2 = pulseIn(ECHO_2, HIGH);
-    
-  digitalWrite(TRIG_3, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_3, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_3, LOW);
-  
-  dur_3 = pulseIn(ECHO_3, HIGH);
-    
-  digitalWrite(TRIG_4, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_4, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_4, LOW);
-  
-  dur_4 = pulseIn(ECHO_4, HIGH);
-
-  // convert the time into a distance of centemeters
-  dist_1 = microsecondsToCentimeters(dur_1);
-  dist_2 = microsecondsToCentimeters(dur_2);
-  dist_3 = microsecondsToCentimeters(dur_3);
-  dist_4 = microsecondsToCentimeters(dur_4);
-  
-  //Test distances to be within range
-  //if in range, change switch to ON (1)
-
-  if ((dist_1 <= MAX_DIST) && (dist_1 >= MIN_DIST)) {
-    switch_1 = 1;
+  // Loop to get distances for each sensor
+  for (int i=3; i < 3 + (2*noButtons); i=i+2) {
+    int n = (i-3)/2; //counter for array
+    pulse_sig(i); //send signal for a sensor
+    dists[n] = microsecondsToCentimeters(pulseIn(i+1, HIGH)); //hold distance in array
   }
-  if ((dist_2 <= MAX_DIST) && (dist_2 >= MIN_DIST)) {
-    switch_2 = 1;
-  }
-  if ((dist_3 <= MAX_DIST) && (dist_3 >= MIN_DIST)) {
-    switch_3 = 1;
-  }
-  if ((dist_4 <= MAX_DIST) && (dist_4 >= MIN_DIST)) {
-    switch_4 = 1;
-  }
-
-  // create array to hold distances and switch conditions
-  float distances[ ] = {dist_1, dist_2, dist_3, dist_4};
-  int switches[ ] = {switch_1, switch_2, switch_3, switch_4}; 
-
-  //loop through switch array to check if they are on
-  for (int i=0; i < sizeof(switches) / sizeof(int); i++) {
-    if (switches[i] == 1) {
-      Serial.print("Switch ");
-      Serial.print(i+1);
-      Serial.print(": On... ");
-      Serial.println(distances[i]);
+  //test distance against max and min
+  for (int s=0; s < noButtons; s++){
+    //if within range, turn switch to on
+    if ((dists[s] <= MAX_DIST) && (dists[s] >= MIN_DIST)) {
+      switches[s] = 1;
+    }
+    //report
+    Serial.print("Sensor ");
+    Serial.print(s+1);
+    if (switches[s] == 1) {
+      Serial.println(" : ON.");
+    }
+    else {
+      Serial.println(" : OFF.");
     }
   }
-
+  
   Serial.println();
-  delay(500);
+  delay(5);
 }
 
+//funtion to calculate distance from microsecond delay
 long microsecondsToCentimeters(long microseconds)
 {
   // The speed of sound is 340 m/s or 29 microseconds per centimeter.
   // The signal travels out and back, so to find the distance of the
   // object we take half of the distance travelled.
   return microseconds / 29 / 2;
+}
+//function to send signal for sensor
+void pulse_sig(int j)
+{
+  digitalWrite(j, LOW);
+  delayMicroseconds(2);
+  digitalWrite(j, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(j, LOW);
 }
